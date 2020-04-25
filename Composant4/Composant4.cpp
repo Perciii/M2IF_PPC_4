71f10b04c26878ee5f3f5a18dbc20fca42bd44f9
Mickael PEEREN <mickael.peeren@dauphine.eu>
2020-04-25 14:53:59 +0200

    Change Prject structure

---
+++ Composant4/Composant4.cpp
@@ -0,0 +1,12 @@
+﻿// Composant4.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Composant4.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
